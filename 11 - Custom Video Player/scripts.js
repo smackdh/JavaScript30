9ea0ee86c6ad5d7c596e239c3cf85115dcbc0a66
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-04 07:05:01 +0900

    added listeners for buttons

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,8 +1,30 @@
 /* Get our elements */
+const player = document.querySelector(".player");
+const video = document.querySelector(".viewer");
+const play = document.querySelector(".toggle");
+const ranges = document.querySelector(".player__slider");
+const skipButtons = document.querySelectorAll("[data-skip]");
+// const playbackRate = document.querySelector("[name=playbackRate]");
+// const rewind10 = document.querySelector("[data-skip='-10']");
 
-play = document.querySelector(".toggle");
-console.log(play);
+console.log(video);
 
 /*Build out functions */
 
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
+
+function updateButton() {
+  // e.type === "play" ? (play.innerHTML = "⏸️") : (play.innerHTML = "▶️");
+  const icon = this.paused ? "▶️" : "⏸️";
+  play.textContent = icon;
+}
+
 /* Hook up the event listeners */
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+
+play.addEventListener("click", togglePlay);
